7a0ad9e1a654e25103d3d6d22bd02ef1f57c3dea
Ahmed Muawia <amuawia666@gmail.com>
2024-12-07 16:53:30 +0200

    0. Redis utils

--- utils/redis.js
+++ utils/redis.js
@@ -1,29 +1,34 @@
 import { createClient } from 'redis';
+import { promisify } from 'util';
 
 class RedisClient {
-  constructor() {
+  constructor () {
     this.client = createClient();
     this.client.on('error', (err) => {
-      console.log(err);
-    });
+        console.log(err);
+      });
+
+    this.getAsync = promisify(this.client.GET).bind(this.client);
+    this.setexAsync = promisify(this.client.SETEX).bind(this.client);
+    this.delAsync = promisify(this.client.DEL).bind(this.client);
   }
 
   isAlive() {
-    return this.client.connected;
+    return this.client.connected; 
   }
 
   async get(key) {
-    const value = await this.client.get(key);
+    const value = await this.getAsync(key);;
 
     return value;
   }
 
   async set(key, val, duration) {
-    await this.client.setEx(key, duration, val);
+    await this.setexAsync(key, duration, val);
   }
 
   async del(key) {
-    await this.client.del(key);
+    await this.delAsync(key);
   }
 }
 
