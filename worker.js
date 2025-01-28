e3c79f91b48fd8f37a065635f2aa955d9e7d2ab6
Ahmed Muawia <amuawia666@gmail.com>
2025-01-28 16:32:08 +0200

    11. New user - welcome email

--- worker.js
+++ worker.js
@@ -1,12 +1,13 @@
 import Queue from 'bull';
-import dbClient from './utils/db';
 import { promises } from 'fs';
+import { ObjectId } from 'mongodb';
+import dbClient from './utils/db';
 
 const imageThumbnail = require('image-thumbnail');
 
-
-
 const fileQueue = new Queue('fileQueue');
+const userQueue = new Queue('userQueue');
+
 fileQueue.process(async (job) => {
   const { fileId, userId } = job.data;
 
@@ -19,7 +20,7 @@ fileQueue.process(async (job) => {
     throw new Error('Missing userId');
   }
 
-   const file = await dbClient.db.collection('files').findOne({ _id: ObjectId(fileId) , userId: ObjectId(userId)});
+  const file = await dbClient.db.collection('files').findOne({ _id: ObjectId(fileId), userId: ObjectId(userId) });
   if (!file) {
     throw new Error('File not found');
   }
@@ -30,7 +31,7 @@ fileQueue.process(async (job) => {
   const widths = [500, 250, 100];
 
   widths.forEach(async (width) => {
-    option.width = width;
+    options.width = width;
     try {
       const thumbNail = await imageThumbnail(localPath, options);
       await promises.writeFile(`${localPath}_${width}`, thumbNail);
@@ -40,4 +41,20 @@ fileQueue.process(async (job) => {
       console.error(err.message);
     }
   });
+});
+
+userQueue.process(async (job) => {
+  const { userId } = job.data;
+
+  if (!userId) {
+    console.log('Missing userId');
+    throw new Error('Missing userId');
+  }
+
+  const user = await dbClient.db.collection('users').findOne({ _id: ObjectId(userId) });
+
+  if (!user) {
+    throw new Error('User not found');
+  }
+  console.log(`Welcome ${user.email}`);
 });
